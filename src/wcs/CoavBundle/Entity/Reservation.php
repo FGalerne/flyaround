176424e96cffe1ab3101ecdb0a749dd38cdb16c9
FGalerne <florian.galerne@free.fr>
2016-12-15 14:18:20 +0100

    crud

---
+++ src/wcs/CoavBundle/Entity/Reservation.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace wcs\CoavBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reservation
+ */
+class Reservation
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var int
+     */
+    private $nb;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Reservation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set nb
+     *
+     * @param integer $nb
+     * @return Reservation
+     */
+    public function setNb($nb)
+    {
+        $this->nb = $nb;
+
+        return $this;
+    }
+
+    /**
+     * Get nb
+     *
+     * @return integer 
+     */
+    public function getNb()
+    {
+        return $this->nb;
+    }
+}
